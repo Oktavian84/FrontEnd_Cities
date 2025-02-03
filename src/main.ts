2ac33956b5f692c71b147adf6311aac73cb012d9
Oktavian Paunku <oggy134@hotmail.com>
2025-02-03 22:33:04 +0100

    Fixed a header and some styleing

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,17 @@ const renderCities = async () => {
 
   allCities.innerHTML = cities.map ((city) => {
     return `
-        <div class="card1 col-8 m-3 mt-3 p-3 border-opacity-50 bg-black text-white">
+        <div class="card1">
           <img src="${city.img_url}" class="img" alt="Bild på ${city.city_name}">
-          <div class="card-body mt-3 candyName">
+          <div class="card-body mt-3">
             <h5 class="text-center city">${city.city_name}</h5>
           </div>
-          <div class="text-center mt-4">
+          <div class="text-center">
             <p>Location: ${city.city_location}</p>
           </div>
-          <div class="card-body d-flex flex-row justify-content-evenly buttonsection">
-            <button class="btn btn-primary btn-lg info-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" data-id-info="${city.id}">Info</button>
+          <div class="card-body d-flex flex-row justify-content-evenly gap-5">
+            <button class="btn btn-primary btn-m info-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" data-id-info="${city.id}">Info</button>
+            <button class="btn btn-danger btn-m delete-btn" type="button" data-id-delete="${city.id}">Delete</button>
           </div>
         </div>`;
       })
